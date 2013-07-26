6ea2b1c1eecbf2a276c9fb23346df7f69810891b
Mike Smullin <mike@smartbombinteractive.com>
2013-07-25 23:06:15 -0600

    holy shit! its callbacks in ANSI C!!!

--- callback.c
+++ callback.c
@@ -25,24 +25,25 @@ function_after_main = (i, cb) ->
 //  when can/can't i change i so that it also changes in main?
 //  when is it a copy of i? when is it not or by reference?
 // but anyway, this for now...
-//void function_must_be_declared_before_main(int i, void(*cb)(char *, int)) { // also works ?
-void function_must_be_declared_before_main(int i, void(*cb)(char [], int)) {
+void function_must_be_declared_before_main(int i, void(*cb)(char *, int)) { // also works!
+//void function_must_be_declared_before_main(int i, void(*cb)(char [], int)) {
   i++;
   cb(NULL, i); // i just almost forgot i was writing ANSI C! :)
 }
 
-//void my_callback(char *err, int number) { // this also works and may be preferable
-void my_callback(char err[], int number) {
+void my_callback(char *err, int number) { // this also works and may be preferable!
+//void my_callback(char err[], int number) {
   if (err != NULL) {
-    printf("Error: %s", err);
+    printf("Error: %s\n", err);
     return;
   }
 
-  printf("number is %i", number);
+  printf("number is %i\n", number);
 }
 
 void main() {
   int x = 1;
   printf("x is %i\n", x);
   function_must_be_declared_before_main(x, my_callback);
+  printf("done.\n");
 }
