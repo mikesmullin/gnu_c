c31783f2e7ed02377ba698864c86100a7535654a
Mike Smullin <mike@smartbombinteractive.com>
2013-07-27 11:08:21 -0600

    document arrow operator

--- classy_struct.c
+++ classy_struct.c
@@ -18,4 +18,11 @@ void main() {
   pet guinea = { "Ariel", "black with white patches" };
   printf("our pet's name is %s and her color is %s\n", dog.name, dog.color);
   printf("our other pet's name is %s and her color is %s\n", guinea.name, guinea.color);
+  // use the dot version when accessing elements of a struct/class
+  // use the Arrow Operator (->) when operating on a pointer to a struct
+  // e.g., foo->bar is equivalent to (*foo).bar
+  pet * alias = &guinea;
+  alias->name = "Red";
+  alias->color = "Strawberry Blonde; red, white, with some dark brown";
+  printf("our other pet's name is %s and her color is %s\n", guinea.name, guinea.color);
 }
