a845133a3a3372cd8b9c98f900bae1c4b25e999a
Mike Smullin <mike@smartbombinteractive.com>
2013-07-28 15:28:03 -0600

    add nested function examples with scoping

---
+++ nested_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void (*f)();
+void (*f2)();
+
+void hamster(void) {
+  f();
+  int x = 22;
+  f2 = ({ void _(void) {
+    printf("x is %i\n", x);
+  } _; });
+}
+
+void b(void) {
+  f2();
+}
+
+void main() {
+  int this = 654;
+  f = ({ void _(void) {
+    printf("this is %i\n", this);
+  } _; });
+  hamster(); // this hasn't fallen out of scope
+  f2(); // but x has fallen out of scope
+  // we can probably get lucky and still reference
+  // our variable, but its unwise
+  // so this is an important difference from javascript
+  // see also:
+  // http://gcc.gnu.org/onlinedocs/gcc/Nested-Functions.html
+}
