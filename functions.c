326fc6351dfb7068b6abfda346d87e9a9023ec37
Mike Smullin <mike@smartbombinteractive.com>
2013-07-28 09:49:48 -0600

    function notes

---
+++ functions.c
@@ -0,0 +1,11 @@
+void main() {
+  unsigned long int a = 10L;
+}
+
+//void include?() { // error: expected something else before '?' token
+void include_question_mark() {
+}
+
+//void gsub!() { // error: expected something else before '!' token
+void gsub_exclaimation_mark() {
+}
