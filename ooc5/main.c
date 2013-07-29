fa8e7734da7a73c6d43730e071851a3909fd48e7
Mike Smullin <mike@smartbombinteractive.com>
2013-07-28 18:56:46 -0600

    add notes

--- ooc5/main.c
+++ ooc5/main.c
@@ -2,6 +2,41 @@
 #include <stdio.h>
 
 void main() {
+  /*
+  what i want, in coffeescript:
+
+  class A
+    @a: -> 1
+    a: -> 2
+    b: -> 3
+    @c: -> 4
+
+  console.log '--'
+  console.log A.a() # class has a class method (pre-override)
+  console.log A.c() # class has a class method (won't be overriden)
+  console.log A.prototype # class has prototype object, b method
+  console.log A.prototype.b() # can execute class prototype's b without instantiation
+  console.log (new A).a() # object has overridden a instance method
+  console.log (new A).b() # object has b method
+  console.log (new A).class # object has no class attribute
+  console.log (new A).prototype # object has no prototype attribute
+  console.log (new A).c() # object #<A> has no method 'c'
+
+  c = ->
+  Object.a = 1
+  Object.prototype.b = 2
+  f = -> 1
+  console.log '--'
+  console.log f() #  1
+  console.log f.a # undefined
+  console.log f.b # 2
+  console.log Object.a # 1
+  console.log Object.prototype.b # 2
+  console.log f.c # undefined
+
+  so it should be like:
+  */
+
   // new function object with empty constructor
   struct Function A = new_Function(lambda(void *, (const struct Function this, int va_length, ...) {
     printf("constructor called.\n");
@@ -29,7 +64,7 @@ void main() {
 
   //int (*f)(void) = A.get(&A, "a");
   struct Function * f = A.get(&A, "a");
-  int ii = f->call(*f, 0);
+  int ii = f->call(*f, 0); // fails because call doesn't know the lambda return type
   printf("%i\n", ii);
 
   //f = A.get(&A, "c");
